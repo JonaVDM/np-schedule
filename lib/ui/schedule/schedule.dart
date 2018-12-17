4adcef78ff77f2ef38ead2c534d4d6d7b8c723e4
JonaVDM <jonathanjvdmeulen@gmail.com>
2018-12-17 20:35:53 +0100

    More then one day

--- lib/ui/schedule/schedule.dart
+++ lib/ui/schedule/schedule.dart
@@ -2,17 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:amo_schedule/models/schedule.dart' as Model;
 import 'package:amo_schedule/ui/schedule/day_slide.dart';
 
-class Schedule extends StatelessWidget {
+class Schedule extends StatefulWidget {
   final Model.Schedule _schedule;
 
   Schedule(this._schedule);
 
   @override
+  ScheduleState createState() {
+    return new ScheduleState();
+  }
+}
+
+class ScheduleState extends State<Schedule> with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    _controller = TabController(
+      length: widget._schedule.perDay().length,
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: DaySlide(
-        _schedule.perDay()[0].lessons,
-      ),
+    return TabBarView(
+      children: widget._schedule.perDay().map((d) => DaySlide(d.lessons)).toList(),
+      controller: _controller,
     );
   }
 }
