6d1f67acb2a9e3c59f2cfa555ecbb0bf6c31bd4f
JonaVDM <jonathanjvdmeulen@gmail.com>
2018-12-18 09:57:16 +0100

    Better looking lesson

--- lib/ui/schedule/day_slide.dart
+++ lib/ui/schedule/day_slide.dart
@@ -6,15 +6,36 @@ class DaySlide extends StatelessWidget {
   final model.Day _day;
   DaySlide(this._day);
 
+  List<Widget> _lessons() {
+    List<Widget> l = [];
+    l.add(Divider());
+    for (model.Lesson les in _day.lessons) {
+      l.add(Lesson(les));
+      l.add(Divider());
+    }
+    return l;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(10),
       child: Column(
         children: <Widget>[
-          Text(_day.toString()),
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 10.0
+            ),
+            child: Text(
+              _day.toString(),
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+          ),
           Expanded(
             child: ListView(
-              children: _day.lessons.map((les) => Lesson(les)).toList(),
+              children: _lessons(),
             ),
           ),
         ],
--- lib/ui/schedule/lesson.dart
+++ lib/ui/schedule/lesson.dart
@@ -8,73 +8,22 @@ class Lesson extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> _days = [
-      'sunday',
-      'monday',
-      'tuesday',
-      'wednesday',
-      'thursday',
-      'friday',
-      'saturday',
-    ];
-
     return Container(
-      margin: EdgeInsets.only(
-        left: 16.0,
-        right: 16.0,
-        top: 7.0,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.deepPurple,
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.circular(12.0),
-      ),
       child: Row(
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 15.0,
-            ),
-            child: Row(
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Container(
-                  width: 190.0,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        _lesson.name ?? _lesson.summary ?? 'Blame the def',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        _lesson.classRoom,
-                        style: TextStyle(
-                          fontSize: 17.0,
-                        ),
-                      ),
-                    ],
-                  ),
+                Text(
+                  _lesson.name ?? _lesson.summary ?? 'blame the def',
+                  style: TextStyle(fontSize: 18.0),
                 ),
-                Container(
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        _days[_lesson.startTime.weekday],
-                      ),
-                      Container(width: 5.0),
-                      Text(
-                        _lesson.getTime(),
-                      ),
-                    ],
-                  ),
-                ),
+                Text(_lesson.classRoom ?? '-'),
               ],
             ),
           ),
+          Text(_lesson.getTime()),
         ],
       ),
     );
