5a4ff4390a6fe8eae007343c007efb4618910fd4
JonaVDM <jonathanjvdmeulen@gmail.com>
2018-12-19 21:19:38 +0100

    Added in filter

--- lib/ui/select/class_list.dart
+++ lib/ui/select/class_list.dart
@@ -13,6 +13,37 @@ class ClassList extends StatefulWidget {
 }
 
 class ClassListState extends State<ClassList> {
+  TextEditingController _controller = TextEditingController();
+  List<model.SchoolClass> _filtered;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_update);
+    _filtered = widget.list;
+  }
+
+  void _update() {
+    setState(() {
+      if (_controller.text.isEmpty) {
+        _filtered = widget.list;
+      } else {
+        _filtered = [];
+        for (var c in widget.list) {
+          if (c.name.contains(_controller.text.toLowerCase()) || c.name.contains(_controller.text.toUpperCase())) {
+            _filtered.add(c);
+          }
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   final Container _divider = Container(
     padding: EdgeInsets.all(5),
     child: Divider(),
@@ -21,7 +52,7 @@ class ClassListState extends State<ClassList> {
   List<Widget> _list() {
     List<Widget> _l = [];
 
-    for (model.SchoolClass c in widget.list) {
+    for (model.SchoolClass c in _filtered) {
       _l.add(ListTile(
         title: Text(c.name),
       ));
@@ -38,15 +69,11 @@ class ClassListState extends State<ClassList> {
         Container(
           padding: EdgeInsets.all(5),
           child: TextField(
-            style: TextStyle(
-              fontSize: 22.0
-            ),
-            decoration: InputDecoration(
-              hintText: 'Search'
-            ),
+            style: TextStyle(fontSize: 22.0),
+            decoration: InputDecoration(hintText: 'Search'),
+            controller: _controller,
           ),
         ),
-
         Expanded(
           child: ListView(
             children: _list(),
