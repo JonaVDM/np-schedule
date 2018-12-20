421aa160573b87d9b65ea500fdbeece3d5d376cb
JonaVDM <jonathanjvdmeulen@gmail.com>
2018-12-20 13:30:00 +0100

    Fixed time

--- lib/models/schedule.dart
+++ lib/models/schedule.dart
@@ -65,10 +65,10 @@ class Lesson {
   });
 
   String getTime() {
-    String startHour = (startTime.hour < 10) ? '0' : '' + startTime.hour.toString();
-    String startMinute = (startTime.minute < 10) ? '0' : '' + startTime.minute.toString();
-    String endHour = (endTime.hour < 10) ? '0' : '' + endTime.hour.toString();
-    String endMinute = (endTime.minute < 10) ? '0' : '' + endTime.minute.toString();
+    String startHour = ((startTime.hour < 10) ? '0' : '') + startTime.hour.toString();
+    String startMinute = ((startTime.minute < 10) ? '0' : '') + startTime.minute.toString();
+    String endHour = ((endTime.hour < 10) ? '0' : '') + endTime.hour.toString();
+    String endMinute = ((endTime.minute < 10) ? '0' : '') + endTime.minute.toString();
     return '$startHour:$startMinute-$endHour:$endMinute';
   }
 }
