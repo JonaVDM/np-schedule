ca5d0a19181bcc79c6e899aa916c6b18d93f311d
JonaVDM <jonathanjvdmeulen@gmail.com>
2018-12-21 10:22:09 +0100

    Saving the classlist

--- lib/models/classes.dart
+++ lib/models/classes.dart
@@ -2,6 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:io';
 import 'package:amo_schedule/file.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SchoolClass {
   String id;
@@ -18,35 +19,39 @@ Future<List<SchoolClass>> fetch() async {
   String url = 'https://sa-nprt.xedule.nl/api/group/';
   String userId = '1f043ffb-68c4-4bf4-ab14-55cf0f500e9e';
   List<SchoolClass> classes = [];
-
-  try {
-    http.Response req =
-        await http.get(url, headers: {'Cookie': 'User=$userId'});
-    var _json = json.decode(req.body);
-    for (var j in _json) {
-      classes.add(SchoolClass.fromJson(j));
+  File f = await localFile('classes.txt');
+  bool excist = await f.exists();
+  var _json;
+  if (!excist) {
+    try {
+      http.Response req =
+          await http.get(url, headers: {'Cookie': 'User=$userId'});
+      f.writeAsString(req.body);
+      _json = json.decode(req.body);
+    } catch (e) {
+      print(e);
     }
-  } catch (e) {
-    print(e);
+  } else {
+    var contents = await f.readAsString();
+    _json = json.decode(contents);
+  }
+
+  for (var j in _json) {
+    classes.add(SchoolClass.fromJson(j));
   }
 
   return classes;
 }
 
-Future<File> save(SchoolClass c) async {
-  File file = await localFile('class-selected.txt');
-  return file.writeAsString('${c.id}\n${c.name}');
+Future<void> save(SchoolClass c) async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setString('classname', c.name);
+  prefs.setString('classId', c.id);
 }
 
 Future<SchoolClass> read() async {
-  try {
-    File file = await localFile('class-selected.txt');
-    String contents = await file.readAsString();
-    var c = contents.split('\n');
-    if (c.length > 1) {
-      return SchoolClass(c[0], c[1]);
-    }
-  } catch (e) {
-    return null;
-  }
+  final prefs = await SharedPreferences.getInstance();
+  final className = prefs.getString('classname') ?? 'AMO17_2KUN';
+  final classId = prefs.getString('classId') ?? '4013';
+  return SchoolClass(classId, className);
 }
---
+++ lib/models/classroom.dart
@@ -0,0 +1,9 @@
+import 'package:http/http.dart';
+import 'dart:io';
+import 'package:amo_schedule/file.dart';
+
+class ClassRoom {
+
+}
+
+
