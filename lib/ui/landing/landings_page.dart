621dc2a44f83fe5179c166688e832441fad2c361
Jonathan <jonathanjvdmeulen@gmail.com>
2019-01-17 08:01:21 +0100

    Basic landings page

---
+++ lib/ui/landing/landings_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LandingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          'Welcome',
+          style: TextStyle(
+            fontSize: 22.0
+          ),
+        ),
+      ),
+    );
+  }
+}
