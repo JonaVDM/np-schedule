2b636f5baa776cf30e2c81daf2009865411993ac
Jonathan <jonajvdm@gmail.com>
2019-02-07 06:59:47 +0100

    Changed dates
    So how it turns out, jan 4 is sometimes in week 2, so changed it up to jan 1

--- lib/util/weeknumber.dart
+++ lib/util/weeknumber.dart
@@ -1,12 +1,12 @@
 int weeknumber(DateTime today) {
-  // Weird thing, 4 January is always in week 1 apparently
-  DateTime jan4 = DateTime(today.year, 1, 4);
+  // Weird thing, 1 January is always in week 1 apparently
+  DateTime jan1 = DateTime(today.year, 1, 1);
 
   // use thursday to compare
   DateTime thursday = today.subtract(Duration(days: (today.weekday - 4)));
 
   // Thursday in week 1
-  DateTime first = jan4.subtract(Duration(days: (jan4.weekday - 4)));
+  DateTime first = jan1.subtract(Duration(days: (jan1.weekday - 4)));
 
   // get the difference and add 1
   return 1 + (thursday.difference(first).inDays / 7).round();
