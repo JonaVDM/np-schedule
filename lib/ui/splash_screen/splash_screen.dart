3c0b1cc97bbb0c3d23d4ef78852d6a43a590d189
Jonathan <jonajvdm@gmail.com>
2019-02-28 18:48:12 +0100

    Switchts to home after 5 seconds

--- lib/ui/splash_screen/splash_screen.dart
+++ lib/ui/splash_screen/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flux/flutter_flux.dart';
 import 'package:np_schedule/stores/schedule/store.dart';
+import 'dart:async';
 
 class SpalshScreen extends StatefulWidget {
   @override
@@ -11,16 +12,26 @@ class _SpalshScreenState extends State<SpalshScreen>
     with StoreWatcherMixin<SpalshScreen> {
   ScheduleStore store;
 
+  void startTimer() {
+    Duration _duration = new Duration(seconds: 5);
+    Timer(_duration, switchToHome);
+  }
+
   @override
   void initState() {
     super.initState();
     store = listenToStore(scheduleStoreToken, checkStore);
+    startTimer();
+  }
+
+  void switchToHome() {
+    Navigator.of(context).pushReplacementNamed('/home');
   }
 
   void checkStore(Store s) {
     setState(() {
       if (store != null && store.classes != null) {
-        Navigator.of(context).pushReplacementNamed('/home');
+        switchToHome();
       }
     });
   }
